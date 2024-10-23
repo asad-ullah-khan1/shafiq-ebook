2b6ad4f4bae5067022ce3ef5c6c2f8d697f975c7
asad-ullah-khan1 <asadkhan602@gmail.com>
2024-10-23 22:38:21 +0500

    fixed few things

--- src/app/components/Layout.js
+++ src/app/components/Layout.js
@@ -42,6 +42,14 @@ export default function Layout({ children }) {
                                         Admin
                                     </Link>
                                 )}
+                                {session?.user?.paymentStatus === 'approved' && (
+                                    <Link
+                                        href="/ebook"
+                                        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                    >
+                                        Ebook
+                                    </Link>
+                                )}
                             </div>
                         </div>
                         <div className="hidden sm:ml-6 sm:flex sm:items-center">
